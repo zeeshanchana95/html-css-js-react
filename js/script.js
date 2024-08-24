03d1528e138673b5d10bcee7a26d9824fd43ed02
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-08-25 01:17:55 +0500

    add second dom practice

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
-let h2 = document.getElementsByTagName("h2");
-h2[0].innerText += " from apna college";
+// let h2 = document.getElementsByTagName("h2");
+// h2[0].innerText += " from apna college";
+
+let divs = document.querySelectorAll(".box");
+console.dir(divs);
+
+divs.forEach((div) => {
+  div.innerText = "new box";
+});
