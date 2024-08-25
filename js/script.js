3152b596d6c99c5259135669a33a363aeca4dcc6
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-08-25 15:23:41 +0500

    add append() and appendChild() methods use in dom

--- js/script.js
+++ js/script.js
@@ -18,3 +18,28 @@ document.querySelector("body").prepend(newHeading);
 
 let para = document.querySelector("p");
 para.remove();
+
+// append() vs appendChild()
+
+const parent = document.createElement("div");
+const child = document.createElement("p");
+
+// 1. .append accepts Node objects and DOMStrings while .appendChild accepts only Node objects
+// Appending Node Objects
+parent.append(child); // Works fine
+parent.appendChild(child); // Works fine
+// Appending DOMStrings
+parent.append("Hello world"); // Works fine
+parent.appendChild("Hello world"); // Throws error
+
+//2. .append does not have a return value while .appendChild returns the appended Node object
+const appendValue = parent.append(child);
+console.log(appendValue); // undefined
+const appendChildValue = parent.appendChild(child);
+console.log(appendChildValue); // <p><p>
+
+//3. .append allows you to add multiple items while appendChild allows only a single item
+const childTwo = document.createElement("p");
+parent.append(child, childTwo, "Hello world"); // Works fine
+parent.appendChild(child, childTwo, "Hello world");
+// Works fine, but adds the first element and ignores the rest
