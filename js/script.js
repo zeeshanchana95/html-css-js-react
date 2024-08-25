8804f18a0419460b76678d2907adb6505b3847f3
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-08-25 17:07:31 +0500

    add event handling in js
    ;

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-// Q 01: Qs. Create a new button element. Give it a text "click me", background color of red & text color of white. Insert the button as the first element inside the body tag.
-let button = document.createElement("button");
-button.innerText = "click me";
+let btn1 = document.querySelector("#btn1");
+let a = 25;
+btn1.onclick = () => {
+  console.log("Handler1");
+};
+btn1.onclick = () => {
+  console.log("Handler2");
+};
 
-button.style.backgroundColor = "red";
-button.style.color = "white";
-
-document.querySelector("body").prepend(button);
-
-// Q 02: Create a <p> tag in html, give it a class & some styling. Now create a new class in CSS and try to append this class to the element. Did you notice, how you overwrite the class name when you add a new one? Solve this problem using classList.
-let p = document.querySelector("p");
-p.classList.add("newClass");
-p.classList.remove("content");
+let box = document.querySelector("div");
+box.onmouseover = () => {
+  console.log("you are inside div");
+};
