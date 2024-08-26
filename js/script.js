f21b68045e09db1b34202daea93dc667c1633a6d
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-08-26 09:55:35 +0500

    add inheritance example in js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,38 @@
-// Inheritance: passing down properties and methods from parent class to child classes
+class Person {
+  constructor() {
+    this.species = "homo sapiens";
+  }
+  eat() {
+    console.log("eact");
+  }
+  sleep() {
+    console.log("sleep");
+  }
 
-class Parent {
-  hello() {
-    console.log("Hello");
+  work() {
+    console.log("do nothing");
   }
 }
 
-class Child extends Parent {}
+class Engineer extends Person {
+  work() {
+    console.log("solve problems, build something");
+  }
+}
+class Doctor extends Person {
+  work() {
+    console.log("treat patients");
+  }
+}
 
-let obj = new Child();
-obj.hello(); //Hello
+let perObj1 = new Person();
+perObj1.work();
+console.dir(perObj1);
+
+let engObj1 = new Engineer();
+console.dir(engObj1);
+engObj1.sleep();
+engObj1.work();
+
+let docObj1 = new Patient();
+docObj1.work();
