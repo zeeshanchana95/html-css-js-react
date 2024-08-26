7439ae1e9d3381ea88a48e0c7c392d7df14ac19f
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-08-26 10:13:31 +0500

    add another example of inheritance and polymorphism

--- js/script.js
+++ js/script.js
@@ -9,6 +9,16 @@ class User {
   }
 }
 
+class Admin extends User {
+  constructor(name, email) {
+    super(name, email);
+  }
+
+  editData() {
+    DATA = "some new value";
+  }
+}
+
 let student1 = new User("zeeshan", "zeeshan@gmail.com");
 student1.viewData();
 
@@ -17,3 +27,7 @@ student1.viewData();
 
 let teacher1 = new User("dean", "dean@gmail.com");
 teacher1.viewData();
+
+let admin1 = new Admin("admin", "admin@gmail.com");
+admin1.editData();
+admin1.viewData();
