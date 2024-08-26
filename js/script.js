75663430e945faba4b85349b53259cc7b10b39e9
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-08-26 09:42:30 +0500

    add classes, constructor, objects, methods, variables in js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
-const employee = {
-  calcTax() {
-    console.log("tax rate is 10%");
-  },
-};
+//class is a blueprint for creating objects
 
-console.log(employee.calcTax());
+// constructor() is a method in js which is automatically invoked when creating a new object
 
-const ahmed = {
-  salary: 50000,
-  calcTax() {
-    console.log("tax rate is 20%");
-  },
-};
+class ToyotaCar {
+  constructor(brand, milege) {
+    console.log("creating new object");
+    this.brand = brand;
+    this.milege = milege;
+  }
 
-ahmed.__proto__ = employee;
+  start() {
+    console.log("start");
+  }
 
-// prototype __proto__ is a special js property that each js object has which is basically reference of other object
-// *If object & prototype have same method, object's method will be used.
+  stop() {
+    console.log("stop");
+  }
+}
+
+let fortuner = new ToyotaCar("fortuner", 10); //constructor invoked
+console.log(fortuner);
+let lexus = new ToyotaCar("lexus", 12); //constructor invoked
+console.log(lexus);
