81b4009519fc09d21ba8b000e53dc78f7aab687a
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-08-26 16:57:39 +0500

    add synchronous and asynchronous programming example

--- js/script.js
+++ js/script.js
@@ -1,22 +1,18 @@
-//Error Handling
+// function hello() {
+//   console.log("Hello");
+// }
+// setTimeout(hello, 4000);
 
-//in js if there is any error occur in the middle of the code, program stop execution at that point. So, we need to handle the error so that our program can continue execution and for this we use Error Handling strategy using try-catch (Exception Handling strategy)
+//synchronous programming: where one instruction waits for other instruction to complete before moving forward
 
-let a = 5;
-let b = 10;
-console.log("a = ", a);
-console.log("b = ", b);
-console.log("a + b = ", a + b);
-try {
-  console.log("a + b = ", a + c); //error
-} catch (err) {
-  console.log(err);
-}
-console.log("a + b = ", a + b);
-console.log("a + b = ", a + b);
-console.log("a + b = ", a + b);
-console.log("a + b = ", a + b);
-console.log("a + b = ", a + b);
-console.log("a + b = ", a + b);
-console.log("a + b = ", a + b);
-console.log("a + b = ", a + b);
+//asynchronous programming: where instructions don't wait for other instruction to complete before moving forward
+
+console.log("one");
+console.log("two");
+
+setTimeout(() => {
+  console.log("hello");
+}, 4000);
+
+console.log("three");
+console.log("four");
